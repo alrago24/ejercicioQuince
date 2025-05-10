41f916b6da563cbbac07367615a6262172d16c1d
alrago24 <alinson_04@hotmail.com>
2025-05-10 11:45:08 -0500

    Se realiza pequeña modificacion en nombre de variable

--- ejercicioQuince/src/App.java
+++ ejercicioQuince/src/App.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String nombre;
+        String nombreCliente;
         String clave;
         System.out.println("Por favor ingresa tu nombre de usuario: ");
-        nombre = sc.nextLine();
+        nombreCliente = sc.nextLine();
         System.out.println("Por favor ingresar: ");
         clave = sc.nextLine();
-        if (nombre.equals("admin") && clave.equals("clave123")) {
+        if (nombreCliente.equals("admin") && clave.equals("clave123")) {
             System.out.println("Bienvenido");
         } else {
             System.out.println("Datos incorrectos");
